d0526342f3ca1511edef9857ed555bdda2da3f66
TatsuyaSagara <tatsuya.sagara@icloud.com>
2023-12-28 02:07:31 +0900

    🐛 ヘッドラインの表示崩れ修正

--- components/headline.js
+++ components/headline.js
@@ -1,16 +1,13 @@
-import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export function Headline(props) {
   console.log(props.title);
   return (
     <div className={styles.description}>
       <p>
-        <h1 className={styles.title}>{props.page} Page</h1>
-        <code className={styles.description}>pages/{props.page}.js</code>
+        Get started by editing&nbsp;
+        <code className={styles.code}>pages/{props.page}.js</code>
       </p>
       <div>
         <a
