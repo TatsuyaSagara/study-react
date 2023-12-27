7f4f220e1ce252367122a6d5d91e05c080fd4c08
TatsuyaSagara <tatsuya.sagara@icloud.com>
2023-12-28 00:40:03 +0900

    🐛 Footerのインポート文追加

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Links } from '../components/links.js'
+import { Footer } from '../components/footer.js'
 
 const inter = Inter({ subsets: ['latin'] })
 
