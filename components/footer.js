3de49a2d2593da2ada355d13238afd5bfd45099f
TatsuyaSagara <tatsuya.sagara@icloud.com>
2023-12-29 11:45:37 +0900

    ♻️ prettierによるフォーマット修正

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,5 @@
-import styles from '@/styles/Home.module.css'
+import styles from "@/styles/Home.module.css";
 
 export function Footer() {
-  return (
-    <footer className={styles.footer}>
-      Thanks by Vercel
-    </footer>
-  )
+  return <footer className={styles.footer}>Thanks by Vercel</footer>;
 }
